6cc4881978c0c3f6893bd5c588fc62a06721a88d
Your Name <you@example.com>
2022-11-24 18:55:25 +0300

    add js file

---
+++ buttons/script.js
@@ -0,0 +1,12 @@
+function Col() {
+  document.getElementById("bod").style.backgroundColor = "blue"
+}
+function Col2() {
+  document.getElementById("bod").style.backgroundColor = "red"
+}
+function Col3() {
+  document.getElementById("bod").style.backgroundColor = "green"
+}
+function Col4() {
+  document.getElementById("bod").style.backgroundColor = "orange"
+}
