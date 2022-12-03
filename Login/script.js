b8c0d04fea7f9f1228e6db0b65b81956ee749e3d
Your Name <you@example.com>
2022-12-03 11:50:05 +0300

    update

---
+++ Login/script.js
@@ -0,0 +1,5 @@
+function Click() {
+  document.getElementById('MoveLeft').style.marginRight = '1000px'
+  document.getElementById('FrontOnly').style.opacity = '1'
+  document.getElementById('FrontOnly').style.letterSpacing = '2px'
+}
